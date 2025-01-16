9e1b7cb4fa8f15a14bfcdc9977ab9516ae264374
RoquePE921204 <roque.p.e.921204@gmail.com>
2025-01-15 19:29:14 -0600

    add enviroment usage

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api',
+  loginApi: 'http://127.0.0.1:8080/user',
+  clientApi: 'http://127.0.0.1:8080/client',
+  insuranceApi: 'http://127.0.0.1:8080/insurance',
+};
